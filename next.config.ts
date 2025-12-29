b3b17d953a38e1231cfefaf1af4f2a19cc66a1a3
coderxwx <623989195@qq.com>
2025-12-29 09:21:24 +0800

    feat: 忽略ts类型检查

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // ⚠️ 警告：这将忽略所有类型错误，仅建议在演示 Demo 时使用
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
